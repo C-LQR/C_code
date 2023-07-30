09a97a4f3b4b1b746124ab295de26fbe2259e6ec
LQR <2537302340@qq.com>
2023-07-30 16:16:53 +0800

    modify contact

--- contact/contact.c
+++ contact/contact.c
@@ -1,19 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
 #include "contact.h"
 
-//Initpeople函数的定义
-void Initpeople(struct Infoarr* ps)
+//初始化函数的定义
+void InitContact(struct Contact* ps)
 {
-	memset(ps, 0, sizeof(ps->data));
-	ps->size = 0;
+	memset(ps->data, 0, sizeof(ps->data));
+	ps->size = 0;//设置通讯录最初只有0个元素
 }
 
-//Add函数的定义
-void Add(struct Infoarr* ps)
+//添加函数的定义
+void AddContact(struct Contact* ps)
 {
 	if (MAX == ps->size)
 	{
-		printf("通讯录已满，无法添加");
+		printf("通讯录已满，无法添加\n");
 	}
 	else
 	{
@@ -33,20 +34,20 @@ void Add(struct Infoarr* ps)
 
 }
 
-//Show函数的定义
-void Show(const struct Infoarr* ps)
+//打印函数的定义
+void ShowContact(const struct Contact* ps)
 {
 	if (ps->size == 0)
 	{
-		printf("当前无记录");
+		printf("通讯录为空\n");
 	}
 	else
 	{
-		printf("%-20s\t%-5s\t%-4s\t%-10s\t%-12s\n", "名字", "年龄", "性别", "地址", "电话");
+		printf("%-20s\t%-4s\t%-5s\t%-20s\t%-12s\n", "名字", "年龄", "性别", "地址", "电话");
 		int i = 0;
 		for (i = 0; i < ps->size; i++)
 		{
-			printf("%-20s\t%-5d\t%-4s\t%-10s\t%-12s\n",
+			printf("%-20s\t%-4d\t%-5s\t%-20s\t%-12s\n",
 				ps->data[i].name,
 				ps->data[i].age,
 				ps->data[i].sex,
--- contact/contact.h
+++ contact/contact.h
@@ -1,17 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-//#pragma once
-#include <string.h>
-#include <stdio.h>
-
 #define MAX 1000
+
 #define MAX_NAME 20
 #define MAX_SEX 5
-#define MAX_ADDR 10
+#define MAX_ADDR 30
 #define MAX_TELE 12
 
+#include <stdio.h>
+#include <string.h>
+
 //声明数据项类型
-struct Info
+struct PeoInfo
 {
 	char name[MAX_NAME];
 	int age;
@@ -20,20 +20,20 @@ struct Info
 	char tele[MAX_TELE];
 };
 
-//声明整体数据类型
-struct Infoarr
+//声明通讯录类型
+struct Contact
 {
-	struct Info data[MAX];
-	int size;
+	struct PeoInfo data[MAX];//结构体数组
+	int size;//记录当前已经有的元素个数
 };
 
-//初始化
-void Initpeople(struct Infoarr* ps);
+//初始化函数声明
+void InitContact(struct Contact* ps);
 
-//添加
-void Add(struct Infoarr* ps);
+//添加函数声明
+void AddContact(struct Contact* ps);
 
-//打印
-void Show(const struct Infoarr* ps);
+//打印函数声明
+void ShowContact(const struct Contact* ps);
 
 
